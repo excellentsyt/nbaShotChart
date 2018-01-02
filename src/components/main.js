f1db0eb3c96f88e4eee5747abd40b0dd673d034e
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2018-01-02 22:59:27 +1000

    init profile

--- src/components/main.js
+++ src/components/main.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { ShotChart } from "./ShotChart";
 import nba from 'nba';
+import {Profile} from "./Profile"
 
 export class Main extends React.Component {
     state = {
-        playerId: nba.findPlayer("Stephen Curry").playerId
+        playerId: nba.findPlayer("Stephen Curry").playerId,
+        playerInfo: {}
     }
 
     componentDidMount() {
-
+        nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playerInfo);
+            this.setState({
+                playerInfo: playerInfo,
+            });
+        });
     }
 
     render() {
         return (
           <div className="main">
+              <Profile/>
               <ShotChart playerId={this.state.playerId}/>
           </div>
         );
