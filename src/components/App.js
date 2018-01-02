9dc3eefc4590c52a41b75e2964d12411e301b625
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2018-01-02 21:35:13 +1000

    Add ShotChart.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import { TopNavBar } from "./TopNavBar"
+import { Main } from "./main"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNavBar/>
+          <Main/>
       </div>
     );
   }
---
+++ src/components/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ShotChart } from "./ShortChart";
+import nba from 'nba';
+
+export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer("Stephen Curry").playerId
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+          <div className="main">
+              <ShotChart playerId={this.state.playerId}/>
+          </div>
+        );
+    }
+}
