d36784b6e8e29a46ace61021ca74ec486e5c9648
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2018-01-02 20:31:47 +1000

    Folder structure.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-import {TopNavBar} from "./TopNavBar"
+import '../styles/App.css';
+import { TopNavBar } from "./TopNavBar"
 
 class App extends Component {
   render() {
