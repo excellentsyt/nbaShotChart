54ae07d4029b3ab5a5e29dfaab39d16a0a32cd12
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2018-01-03 16:03:29 +1000

    Add debounce.

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ShotChart } from './ShotChart';
 import { CountSlider } from "./CountSlider"
+import _ from 'lodash';
 
 export class DataViewContainer extends React.Component {
     state = {
@@ -18,7 +19,7 @@ export class DataViewContainer extends React.Component {
                 <div className="filters">
                     <CountSlider
                         minCount={this.state.minCount}
-                        onCountSliderChange={this.onCountSliderChange}/>
+                        onCountSliderChange={_.debounce(this.onCountSliderChange, 500)}/>
                 </div>
             </div>
         );
